03ba13c97c3b234d2fbcbcaadb065000d09158f1
clarkeadg <clarkeadg@yahoo.com>
2023-12-21 15:35:54 -0800

    card flip

--- src/components/Countdown/CountdownNumber.tsx
+++ src/components/Countdown/CountdownNumber.tsx
@@ -8,13 +8,27 @@ const CountdownNumber = (props:ICountdownNumber) => {
 
   return (
     <div className="w-[70px] md:w-[150px]">
-      <div className="countdown-card relative rounded-xl overflow-hidden flex items-center justify-center w-full h-[70px] md:h-[140px] mb-4 md:mb-6">
-        <div className="countdown-card-top rounded-t-xl absolute top-0 left-0 w-full h-[35px] md:h-[70px]"/>
-        <div className="countdown-card-circle rounded-full absolute top-50 -left-[4px] md:-left-[8px] w-[8px] h-[8px] md:w-[16px] md:h-[16px]"/>
-        <div className="countdown-card-circle rounded-full absolute top-50 -right-[4px] md:-right-[8px] w-[8px] h-[8px] md:w-[16px] md:h-[16px]"/>
-        <span className="countdown-number text-[40px] md:text-[80px]">
-          { number }
-        </span>
+      <div className="countdown-card-outer pb-2 md:pb-4 rounded-xl mb-4 md:mb-2 relative">
+        <div className="countdown-card relative flex items-center justify-center w-full h-[70px] md:h-[140px]">
+          <div className="countdown-card-inner rounded-xl relative w-full h-[70px] md:h-[140px]">
+            <div className="countdown-card-front rounded-xl overflow-hidden absolute top-0 left-0 w-full h-[70px] md:h-[140px]">
+              <div className="countdown-number flex items-center justify-center absolute top-0 left-0 w-full h-[70px] md:h-[140px] text-[40px] md:text-[80px]">
+                { number }
+              </div>
+              <div className="countdown-card-shadow absolute top-0 left-0 w-full h-[35px] md:h-[70px]"/>
+              <div className="countdown-card-circle rounded-full absolute top-[31px] md:top-[62px] -left-[4px] md:-left-[8px] w-[8px] h-[8px] md:w-[16px] md:h-[16px]"/>
+              <div className="countdown-card-circle rounded-full absolute top-[31px] md:top-[62px] -right-[4px] md:-right-[8px] w-[8px] h-[8px] md:w-[16px] md:h-[16px]"/>
+            </div>
+            <div className="countdown-card-back rounded-xl overflow-hidden absolute bottom-0 left-0 w-full h-[70px] md:h-[140px]">
+              <div className="countdown-number flex items-center justify-center absolute top-0 left-0 w-full h-[70px] md:h-[140px] text-[40px] md:text-[80px]">
+                { "09" }
+              </div>
+              <div className="countdown-card-shadow absolute top-0 left-0 w-full h-[35px] md:h-[70px]"/>
+              <div className="countdown-card-circle rounded-full absolute top-[31px] md:top-[62px] -left-[4px] md:-left-[8px] w-[8px] h-[8px] md:w-[16px] md:h-[16px]"/>
+              <div className="countdown-card-circle rounded-full absolute top-[31px] md:top-[62px] -right-[4px] md:-right-[8px] w-[8px] h-[8px] md:w-[16px] md:h-[16px]"/>
+            </div>          
+          </div> 
+        </div>        
       </div>
       <div className="text-[9px] md:text-[16px] uppercase tracking-[2px] md:tracking-[4px]">
         { title }
